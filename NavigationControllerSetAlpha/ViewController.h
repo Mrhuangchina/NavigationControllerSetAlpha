4d332e22e9bf285b397fa96a6f4dd8f7c247c696
Mrhuangchina <jianmissyoulove@qq.com>
2018-08-24 20:56:22 +0800

    Initial Commit

---
+++ NavigationControllerSetAlpha/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NavigationControllerSetAlpha
+//
+//  Created by MrHuang on 18/8/24.
+//  Copyright © 2018年 Mrhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NavigationControllerSetAlpha/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NavigationControllerSetAlpha
+//
+//  Created by MrHuang on 18/8/24.
+//  Copyright © 2018年 Mrhuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
